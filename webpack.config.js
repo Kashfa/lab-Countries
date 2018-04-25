818f346a79958468c39ec9a5c5ad135bb0293cfc
Kashfa <cyan82@hotmail.com>
2018-04-25 14:49:10 +0100

    added express, webpack, mongodb.  Also created the post function

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+
+const config = {
+ entry: `${__dirname}/client/src/app.js`,
+ output: {
+   path: `${__dirname}/client/public`,
+   filename: 'bundle.js'
+ },
+ mode: 'development'
+};
+
+module.exports = config;
