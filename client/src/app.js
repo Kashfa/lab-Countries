f070d1737e7555605457af758a25b63bab454620
Kashfa <cyan82@hotmail.com>
2018-04-25 17:23:01 +0100

    working on post function

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,8 @@
-
+const Request = require('./services/request.js');
 const CountriesData = require('./models/countries_data.js');
 const CountriesView = require('./views/countries_view.js');
+
+const request = new Request('http://localhost:3000/db/countries');
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -8,6 +10,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const countriesData = new CountriesData();
   const countriesView = new CountriesView(countrySelect);
+
+  countriesView.countrySelect.addEventListener('change', () => {
+    // get index of selected country
+    // get the country
+    request.post(onPostComplete, country); // this function needs writing
+
+  });
 
   countriesData.getData((data) => {
     // console.dir(data);
